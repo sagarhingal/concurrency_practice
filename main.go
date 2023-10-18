3468b3aec273d1bfe256a993e461473a1fc7373f
Sagar Hingal <sagar.hingal@gmail.com>
2023-10-18 15:19:43 +0530

    added basic CLI menu

--- main.go
+++ main.go
@@ -1,12 +1,63 @@
 package main
 
 import (
+	"bufio"
 	"concurrency_practice/patterns"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 
-	go patterns.TestPatterns()
-	time.Sleep(100 * time.Millisecond)
+	// Print the menu
+	menu()
+	fmt.Printf("\nGoodbye!\n\n")
+	os.Exit(1)
+}
+
+func menu() {
+
+	// print the menu
+	fmt.Printf("\n------\nMenu -\n------\n1) Fan-In & Fan-Out\n2) Worker-Pool\n3) Try Again\n4) Exit\n")
+	// now take the user input
+	buf := bufio.NewReader(os.Stdin)
+	fmt.Printf("\n> ")
+	arg, err := buf.ReadBytes('\n')
+	if err != nil {
+		fmt.Printf("error: %v\n\n", err)
+		menu()
+	}
+
+	// clean the string
+	strArg := strings.Trim(string(arg), "\r\n")
+	choice, err := strconv.ParseInt(strArg, 10, 64)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		fmt.Printf("invalid choice: %d\nPlease choose from the following menu:\n\n", choice)
+		menu()
+	}
+
+	// now check the choice and call respective menu option
+	switch choice {
+	case 1:
+		// call fanIn-fanOut
+		go patterns.TestPatterns()
+		time.Sleep(500 * time.Millisecond)
+		menu()
+	case 2:
+		// TODO: call worker-pool
+		fmt.Printf("\nWorker-pool(2) pattern is yet to be implemented...\nPlease choose from these options:\n")
+		menu()
+	case 3:
+		menu()
+	case 4:
+		break
+	default:
+		fmt.Printf("invalid choice: %d\nPlease choose from the following menu:\n", choice)
+		menu()
+	}
+
 }
--- patterns/fanInOut.go
+++ patterns/fanInOut.go
@@ -3,5 +3,5 @@ package patterns
 import "fmt"
 
 func TestPatterns() {
-	fmt.Println("Hi, from Fan-In and Fan-Out")
+	fmt.Printf("\n\nHi, from Fan-In and Fan-Out\n")
 }
