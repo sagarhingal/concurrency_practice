7bea342cfbb3bab801b9ff8338dd49707d20f2e4
Sagar Hingal <sagar.hingal@gmail.com>
2023-10-20 14:23:05 +0530

    added waitgroup

--- patterns/fanInOut.go
+++ patterns/fanInOut.go
@@ -2,44 +2,56 @@ package patterns
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func TryFanInOut() {
 	fmt.Printf("\nHi, from Fan-In and Fan-Out\n")
+	wg := sync.WaitGroup{}
 
 	dishRequest := make(chan string)
 
-	MakeDish(&dishRequest)
+	wg.Add(1)
+	go MakeDish(&dishRequest, &wg)
 
-	go func() {
-		dishRequest <- "sandwich"
-	}()
+	dishRequest <- "sandwich"
 
-	go func() {
-		dishRequest <- "paratha"
-	}()
+	dishRequest <- "paratha"
+
+	wg.Wait()
 }
 
-func MakeDish(dishRequest *chan string) {
+func MakeDish(dishRequest *chan string, wg *sync.WaitGroup) {
 
+	defer wg.Done()
+
+	dishName := <-*dishRequest
+	fmt.Printf("Order received for %s!\n", dishName)
 	myPotatoes := <-getPotato()
 	myVeggies := <-getVeggies()
-	dishName := <-*dishRequest
 
 	if myPotatoes && myVeggies { // lets read the values now
+		fmt.Printf("Got the potatoes and the veggies!\n")
 		switch dishName {
 		case "sandwich":
-			makeSandwich()
+			status := <-makeSandwich()
+			if status {
+				fmt.Printf("Order[%s] is ready!\n", dishName)
+			}
 		case "paratha":
-			makeParatha()
+			status := <-makeParatha()
+			if status {
+				fmt.Printf("Order[%s] is ready!\n", dishName)
+			}
 		default:
 			fmt.Printf("Sorry! Can\\'t make %s, only can make Sandwich & Paratha.", dishName)
 		}
 	}
 }
 
-func makeSandwich() chan bool {
+func makeSandwich() <-chan bool {
+	fmt.Printf("Order[Sandwich] in process...\n")
 	sandwich := make(chan bool)
 
 	sandwich <- true
@@ -47,7 +59,8 @@ func makeSandwich() chan bool {
 	return sandwich
 }
 
-func makeParatha() chan bool {
+func makeParatha() <-chan bool {
+	fmt.Printf("Order[Paratha] in process...\n")
 	paratha := make(chan bool)
 
 	paratha <- true
